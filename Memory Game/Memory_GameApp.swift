c58e984eaab32177d7142c4aff422bac1744e6ae
Jesten Scheideger <jesten.scheideger@okstate.edu>
2025-03-07 16:00:04 -0600

    Initial Commit

---
+++ Memory Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory Game
+//
+//  Created by Jesten Scheideger on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Memory GameTests/Memory_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameTests.swift
+//  Memory GameTests
+//
+//  Created by Jesten Scheideger on 3/7/25.
+//
+
+import Testing
+@testable import Memory_Game
+
+struct Memory_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
